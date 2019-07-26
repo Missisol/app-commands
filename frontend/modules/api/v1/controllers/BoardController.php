3591bac8d068f2648e78c9add909aef589caf407
Galina <denis@isyanov.com>
2019-07-26 13:15:34 +0300

    [Feature #18] User's board - change one board

--- frontend/modules/api/v1/controllers/BoardController.php
+++ frontend/modules/api/v1/controllers/BoardController.php
@@ -3,14 +3,14 @@
 namespace frontend\modules\api\v1\controllers;
 
 use Yii;
-use frontend\modules\api\v1\models\InitViewBoard;
+use frontend\modules\api\v1\models\GetInfoByBoard;
 
 class BoardController extends ApiController
 {
-    public function actionIndex()
+    public function actionIndex($id = null)
     {
         $user_id = Yii::$app->user->identity->getId();
-        $result = (new InitViewBoard($user_id))->initView();
+        $result = (new GetInfoByBoard($user_id, $id))->getInfo();
         return $this->sendResponse(self::STATUS_OK, $result);
     }
 }

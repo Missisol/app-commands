93a9acd54c0c127e9938f2b00cd051d979bff5a3
Galina Is <denis@isyanov.com>
2019-08-03 22:01:42 +0300

    Remove CsrfTokenValidation from main-local env

--- environments/prod/frontend/config/main-local.php
+++ environments/prod/frontend/config/main-local.php
@@ -3,7 +3,8 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            //'cookieValidationKey' => '',
+            'enableCsrfValidation' => false,
         ],
     ],
 ];
